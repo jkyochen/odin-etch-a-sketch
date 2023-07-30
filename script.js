3dad3edb715b7f33466622edf7b5d16a58e39684
ChenZiQiang <ziqiang.chen@outlook.com>
2023-07-30 16:42:04 +0900

    Add support change grid amount

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 
-let container = document.querySelector(".container");
+let container = document.getElementById("container");
+let gridBtn = document.querySelector(".grid-btn");
+let gridAmount = 16;
 
-for (let i = 0; i < 16 * 16; i++) {
-    let grid = document.createElement("div");
-    grid.classList.add("grid");
-    grid.textContent = i + 1;
-    container.appendChild(grid);
+initGrids();
+
+gridBtn.addEventListener("click", (e) => {
+    let newGridAmount = prompt("Please input new grid amount.")
+    newGridAmount = Number(newGridAmount);
+    if (isNaN(newGridAmount)) {
+        alert("Error: grid amount, please input again.");
+        return;
+    }
+    if (newGridAmount > 50) {
+        alert("Error: grid limit 100 in a line.")
+    }
+    gridAmount = newGridAmount;
+    initGrids();
+});
+
+function initGrids() {
+    container.innerHTML = "";
+    for (let i = 0; i < gridAmount * gridAmount; i++) {
+        let grid = document.createElement("div");
+        grid.classList.add("grid");
+        let gridWidth = (container.clientWidth - gridAmount * 2) / gridAmount;
+        grid.style.width = `${gridWidth}px`;
+        grid.style.height = `${gridWidth}px`;
+        container.appendChild(grid);
+    }
 }
